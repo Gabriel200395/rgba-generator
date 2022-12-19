a91f9ba81eac577825df59d1c29403d72ea033f2
Gabriel Souza <eugabrielf@gmail.com>
2022-12-18 21:05:07 -0300

    arrumando codigo

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -4,35 +4,39 @@ import { hours, minutes, dataFormat } from "../../helpers/functions_data";
 import "./style.css";
 
 type CorProps = {
-  rosa: number;
-  verde: number;
-  azul: number;
+  pink: number;
+  green: number;
+  blue: number;
 };
 
 const Home: React.FC = () => {
   const [cores, setCores] = useState<CorProps>({
-    rosa: 93,
-    verde: 121,
-    azul: 184,
+    pink: 93,
+    green: 121,
+    blue: 184,
   });
 
-  const [tema, setTema] = useState(false);
+  const [theme, setTheme] = useState(false);
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>) =>
     setCores({ ...cores, [event.target.name]: event.target.value });
 
-  useEffect(() => {
+  function schedule() {
     if (hours < 18) {
-      setTema(false);
+      setTheme(false);
     }
     if (hours >= 18 && minutes >= 0) {
-      setTema(true);
+      setTheme(true);
     }
+  }
+
+  useEffect(() => {
+    schedule();
   }, [hours, minutes]);
 
   return (
     <div
-      className={`${tema ? "tema-escuro" : "home-p"}`}
+      className={`${theme ? "theme-page" : "page-default"}`}
       data-testid="container-page"
     >
       <h2>Gerador RGB</h2>
@@ -43,36 +47,32 @@ const Home: React.FC = () => {
       <div className="container-rgba">
         <div className="grid-input">
           <Input
-            className="rosa"
-            name="rosa"
-            state={cores.rosa}
+            className="pink"
+            name="pink"
+            state={cores.pink}
             onChange={handleChange}
-            data-testid="rosa"
           />
           <Input
-            className="verde"
-            name="verde"
-            state={cores.verde}
+            className="green"
+            name="green"
+            state={cores.green}
             onChange={handleChange}
-            data-testid="verde"
           />
           <Input
-            className="azul"
-            name="azul"
-            state={cores.azul}
+            className="blue"
+            name="blue"
+            state={cores.blue}
             onChange={handleChange}
-            data-testid="azul"
           />
         </div>
         <div
-          data-testid="container-rgba"
           className="container-radius"
           style={{
-            backgroundColor: `rgba(${cores.rosa},${cores.verde},${cores.azul})`,
+            backgroundColor: `rgba(${cores.pink},${cores.green},${cores.blue})`,
           }}
         />
         <p>
-          rgba({cores.rosa}, {cores.verde}, {cores.azul})
+          rgba({cores.pink}, {cores.green}, {cores.blue})
         </p>
       </div>
     </div>
--- src/__test__/Home.test.tsx
+++ src/__test__/Home.test.tsx
@@ -1,60 +1,63 @@
 import { screen, fireEvent, render } from "@testing-library/react";
 import Home from "../Pages/Home/index";
 
-test("DEFAULT APP", () => {
-  render(<Home />);
+describe("Testing application", () => {
+  test("Application default", () => {
+    render(<Home />);
 
-  const input1 = screen.getByTestId("rosa") as HTMLInputElement;
-  const input2 = screen.getByTestId("verde") as HTMLInputElement;
-  const input3 = screen.getByTestId("azul") as HTMLInputElement;
-  const container = screen.getByTestId("container-rgba");
+    const fieldPink = screen.getByDisplayValue("93");
+    const fieldGreen = screen.getByDisplayValue("121");
+    const fieldBlue = screen.getByDisplayValue("184");
+    const containerText = screen.getByText(/rgba\(93, 121, 184\)/i);
 
-  expect(input1.value).toEqual("93");
-  expect(input2.value).toEqual("121");
-  expect(input3.value).toEqual("184");
-
-  expect(
-    (container.style.backgroundColor = `rgba(${input1.value},${input2.value},${input3.value})`)
-  ).toEqual(`rgba(${input1.value},${input2.value},${input3.value})`);
-});
-
-test("FUNÇÕES APP", () => {
-  render(<Home />);
-
-  const input1 = screen.getByTestId("rosa") as HTMLInputElement;
-  const input2 = screen.getByTestId("verde") as HTMLInputElement;
-  const input3 = screen.getByTestId("azul") as HTMLInputElement;
-  const container = screen.getByTestId("container-rgba");
-
-  fireEvent.change(input1, {
-    target: {
-      value: "93",
-    },
+    [fieldPink, fieldGreen, fieldBlue, containerText].forEach((element) =>
+      expect(element).toBeInTheDocument()
+    );
   });
 
-  fireEvent.change(input2, {
-    target: {
-      value: "121",
-    },
+  test("Event field application", () => {
+    render(<Home />);
+
+    const fieldPink = screen.getByDisplayValue("93");
+    const fieldGreen = screen.getByDisplayValue("121");
+    const fieldBlue = screen.getByDisplayValue("184");
+
+    [
+      {
+        nameField: fieldPink,
+        valueFielde: "93",
+      },
+      {
+        nameField: fieldGreen,
+        valueFielde: "93",
+      },
+      {
+        nameField: fieldBlue,
+        valueFielde: "93",
+      },
+    ].forEach((element) =>
+      fireEvent.change(element.nameField, {
+        target: {
+          value: element.valueFielde,
+        },
+      })
+    );
+
+    const containerText = screen.getByText(/rgba\(93, 93, 93\)/i);
+    expect(containerText).toBeInTheDocument();
   });
 
-  fireEvent.change(input3, {
-    target: {
-      value: "184",
-    },
+  test("Theme page", async () => {
+    render(<Home />);
+
+    let pageDefault = screen
+      .getByTestId("container-page")
+      .classList.contains("page-default");
+    let themPage = screen
+      .getByTestId("container-page")
+      .classList.contains("theme-page");
+
+    expect(pageDefault).toEqual(false);
+    expect(themPage).toEqual(true);
   });
-
-  expect(
-    (container.style.backgroundColor = `rgba(${input1.value},${input2.value},${input3.value})`)
-  ).toEqual(`rgba(${input1.value},${input2.value},${input3.value})`);
-});
-
-test("FUNÇÕES TEMA PAGE", async () => {
-  render(<Home />);
-  expect(
-    screen.getByTestId("container-page").classList.contains("home-p")
-  ).toEqual(false);
-  expect(
-    screen.getByTestId("container-page").classList.contains("tema-escuro")
-  ).toEqual(true);
 });
